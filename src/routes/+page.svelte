e018031d678db3733c8948b22c736f242cd79d6e
edberaga <edbetjonnathan@gmail.com>
2025-01-04 18:31:46 +0700

    Develop the todo app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,19 @@
-<script lang="ts">
-    let name = "Edbert Lim";
-</script>
+<script>
+    let todos = [];
 
-<h1>Hello, My name is {name} </h1>
+    function addTodo() {
+        todos = [...todos, ''];
+    }
+    
+    function removeTodo(index) {
+        todos = [...todos.slice(0, index), ...todos.slice(index + 1)];
+    }
+</script>
+<h1>Todo List</h1>
+{#each todos as todo, index}
+    <input bind:value={todos[index]}>
+    <button on:click={() => removeTodo(index)}>X</button>
+    <br/>
+{/each}
+
+<button on:click={addTodo}>Add Note</button>
