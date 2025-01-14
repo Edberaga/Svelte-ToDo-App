5262fe1bb67a086686e27d65db29fdbd00fcd0eb
edberaga <edbetjonnathan@gmail.com>
2025-01-15 03:39:58 +0700

    Redevelop the Todo App

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,122 +1,16 @@
 <script lang="ts">
-	import { fade } from 'svelte/transition';
+	import DataTable from "./todos/todo-table.svelte";
+  </script>
 
-	type Todo = {
-		text: string
-		done: boolean
-	}
-	type Filters = 'all' | 'active' | 'completed'
-
-	let todos = $state<Todo[]>([])
-	let filter = $state<Filters>('all')
-	let filteredTodos = $derived(filterTodos())
-
-	$effect(() => {
-		const savedTodos = localStorage.getItem('todos')
-		savedTodos && (todos = JSON.parse(savedTodos))
-	})
-
-	$effect(() => {
-		localStorage.setItem('todos', JSON.stringify(todos))
-	})
-
-	function addTodo(event: KeyboardEvent) {
-		if (event.key !== 'Enter') return
-
-		const todoEl = event.target as HTMLInputElement
-		const text = todoEl.value
-		const done = false
-
-		todos = [...todos, { text, done }]
-
-		todoEl.value = ''
-	}
-
-	function editTodo(event: Event) {
-		const inputEl = event.target as HTMLInputElement
-		const index = +inputEl.dataset.index!
-		todos[index].text = inputEl.value
-	}
-
-	function toggleTodo(event: Event) {
-		const inputEl = event.target as HTMLInputElement
-		const index = +inputEl.dataset.index!
-		todos[index].done = !todos[index].done
-	}
-
-	function setFilter(newFilter: Filters) {
-		filter = newFilter
-	}
-
-	function filterTodos() {
-		switch (filter) {
-			case 'all':
-				return todos
-			case 'active':
-				return todos.filter((todo) => !todo.done)
-			case 'completed':
-				return todos.filter((todo) => todo.done)
-		}
-	}
-
-	function remaining() {
-		return todos.filter((todo) => !todo.done).length
-	}
-</script>
-
-<input onkeydown={addTodo} placeholder="Add todo" type="text" />
-
-<div class="todos">
-	{#each filteredTodos as todo, i}
-		<div
-			class:completed={todo.done}
-			class="todo"
-			in:fade={{ duration: 300 }}
-			out:fade={{ duration: 300 }}
-		>
-			<input oninput={editTodo} data-index={i} value={todo.text} type="text" />
-			<input onchange={toggleTodo} data-index={i} checked={todo.done} type="checkbox" />
+<div class="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+	<div class="flex items-center justify-between space-y-2">
+		<div>
+			<h1 class="text-4xl font-bold tracking-tight text-white">Welcome back!</h1>
+			<p class="text-muted-foreground">Here's a list of your tasks for this month!</p>
 		</div>
-	{/each}
+		<div class="flex items-center space-x-2">
+			
+		</div>
+	</div>
+	<DataTable />
 </div>
-
-<div class="filters">
-	<button onclick={() => setFilter('all')}>All</button>
-	<button onclick={() => setFilter('active')}>Active</button>
-	<button onclick={() => setFilter('completed')}>Completed</button>
-</div>
-
-<p>{remaining()} remaining</p>
-
-<style>
-	.todos {
-		display: grid;
-		gap: 1rem;
-		margin-block-start: 1rem;
-	}
-
-	.todo {
-		position: relative;
-		transition: opacity 0.3s;
-	}
-
-	.completed {
-		opacity: 0.4;
-	}
-
-	input[type='text'] {
-		width: 100%;
-		padding: 1rem;
-	}
-
-	input[type='checkbox'] {
-		position: absolute;
-		right: 4%;
-		top: 50%;
-		translate: 0% -50%;
-	}
-
-	.filters {
-		margin-block: 1rem;
-	}
-</style>
