b7617225bf948f298b5c02a52035c70eb2eac9d8
edberaga <edbetjonnathan@gmail.com>
2025-01-19 23:42:23 +0700

    remove the layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
 	import DataTable from "./todos/todo-table.svelte";
-  </script>
+	import '../app.css';
+</script>
+
+<svelte:head>
+	<title>Edbert Todo App</title>
+</svelte:head>
 
 <div class="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
 	<div class="flex items-center justify-between space-y-2">
@@ -14,3 +19,19 @@
 	</div>
 	<DataTable />
 </div>
+
+<style>
+	:global(html, body) {
+		height: 100%;
+	}
+
+	:global(body) {
+		padding-top: 50px;
+		font-family: 'Manrope Variable', sans-serif;
+		background-color: hsl(220 10% 8%);
+	}
+
+	/*Media Queries*/
+	@media (max-width: 768px) {
+    }
+</style>
